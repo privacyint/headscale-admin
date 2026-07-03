ab3d624d1ee33a3d8418942e2eca8ea1e83bed8a
CJFWeatherhead <CJFWeatherhead@users.noreply.github.com>
2026-07-03 12:37:55 +0100

    fix: update version to 0.29.2/update1 in debug module and add workplan to .gitignore

--- src/lib/common/debug.ts
+++ src/lib/common/debug.ts
@@ -1,6 +1,6 @@
 import { App } from "$lib/States.svelte";
 
-export const version = '0.29.2/update0';
+export const version = '0.29.2/update1';
 
 export function debug(...data: unknown[]) {
 	// output if console debugging is enabled
